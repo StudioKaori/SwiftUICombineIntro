e80f375a400e26c8f5d35ee45b0f48026cb51fc7
Studio Kaori <studiokaori.com@gmail.com>
2022-06-10 10:08:43 +0200

    Initial Commit

---
+++ SwiftUICombineIntro/SwiftUICombineIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineIntroApp.swift
+//  SwiftUICombineIntro
+//
+//  Created by Kaori Persson on 2022-06-10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
